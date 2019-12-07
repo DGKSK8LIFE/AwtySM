13e32d555af9a30fdd318839b70d245b7caa08ad
Tarek Ali <tarekali15@outlook.com>
2019-12-06 21:32:04 -0600

    working on the backend

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -7,6 +9,21 @@ app = Flask(__name__)
 def login():
     return render_template("index.html")
 
-@app.route("/loggedin")
-def auth():
-    
+
+@app.route('/loggedin', methods=['POST'])
+def verify_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if not username or not password:
+        return render_template('failpage.html')
+    else:
+        db = sqlite3.connect('accounts.sqlite')
+        query = db.execute(
+            f'SELECT * FROM ACCOUNT WHERE username=\'{username}\' AND password=\'{password}\';')
+        account = query.fetchall()
+        if account:
+            db.close()
+            # allow users to access dashboard here
+        # close db and display a fail message
+        db.close()
+        return render_template('index.html')
