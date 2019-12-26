b863772fe8810b6f5262d714c234bfaed810fa61
Tarek Ali <tarekali15@outlook.com>
2019-12-26 14:35:02 -0600

    cleared db

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 app.secret_key = 'x3964njs2356xa28169asdfmvm'
 
-
 restricted_chars = ('/', ';', '*', '=', '\'', '\"',
                     '#', '<', '>', '[', ']', '{', '}')
 
@@ -43,6 +42,8 @@ def sports():
 
 """ gets username and password -> checks if they contain restricted characters -> 
     validate them in the database -> send to menu """
+
+
 @app.route('/loggedin', methods=['POST'])
 def verify_login():
     username = request.form.get('username')
@@ -66,6 +67,8 @@ def verify_login():
 
 """ gets username & password -> checks to see if they contain illegal characters 
     -> writes the credentials to the accounts.sqlite database -> redirects to login.html """
+
+
 @app.route('/created', methods=['POST'])
 def create_account():
     username = request.form.get('username')
