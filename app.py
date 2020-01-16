db5a3443577cc7ffa2d888915904e9dc91c49c7f
Tarek Ali <tarekali15@outlook.com>
2020-01-15 20:47:38 -0600

    spacing in app.py; added background image, fixing issue

--- app.py
+++ app.py
@@ -90,8 +90,6 @@ def handle_custom_event(json):
 
 """ gets username and password -> checks if they contain restricted characters ->
     validate them in the database -> send to menu """
-
-
 @app.route("/loggedin", methods=["POST", "GET"])
 def verify_login():
     username = request.form.get("username")
@@ -126,8 +124,6 @@ def verify_login():
 
 """ gets username & password -> checks to see if they contain illegal characters 
     -> writes the credentials to the accounts.sqlite database -> redirects to login.html """
-
-
 @app.route("/created", methods=["POST"])
 def create_account():
     username = request.form.get("username")
